ce8435fde2c4881daf35e3a491e456d5f9f024bd
GitFeri <68915209+GitFeri@users.noreply.github.com>
2020-10-31 20:04:02 +0100

    Könyv osztály

---
+++ src/main/java/classsctructureconstructors/BookMain.java
@@ -0,0 +1,12 @@
+package classsctructureconstructors;
+
+public class BookMain {
+    public static void main(String[] args) {
+        Book book = new Book("Szerző","Cím");
+        book.register("123456");
+
+        System.out.println("Szerző: " + book.getAuthor());
+        System.out.println("Cím   : " + book.getTitle());
+        System.out.println("Regnum: " + book.getRegNumber());
+    }
+}
