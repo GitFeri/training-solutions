9a39a0322eb4e9962d8d4fab9110d14055e631ce
GitFeri <68915209+GitFeri@users.noreply.github.com>
2020-10-31 20:22:12 +0100

    Raktár osztály

---
+++ src/main/java/classsctructureconstructors/Store.java
@@ -0,0 +1,38 @@
+package classsctructureconstructors;
+
+public class Store {
+    private String product;
+    private int stock;
+
+    public Store(String product) {
+        setProduct(product);
+        setStock(0);
+    }
+
+    public String getProduct() {
+        return product;
+    }
+
+    public void setProduct(String product) {
+        this.product = product;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public void store (int stock) {
+        setStock(getStock() + stock);
+    }
+
+    public void dispatch (int stock) {
+        setStock(getStock() - stock);
+    }
+
+
+}
+
---
+++ src/main/java/classsctructureconstructors/StoreMain.java
@@ -0,0 +1,18 @@
+package classsctructureconstructors;
+
+public class StoreMain {
+    public static void main(String[] args) {
+        Store store1 = new Store("alma");
+        Store store2 = new Store("körte");
+
+        store1.store(10);
+        store2.store(20);
+
+        store1.dispatch(5);
+        store2.dispatch(9);
+
+        System.out.println("Első termék    : " + store1.getProduct() + " " + store1.getStock() + " db");
+        System.out.println("Második termék : " + store2.getProduct() + " " + store2.getStock() + " db");
+
+    }
+}
