d3ad5730b2c8be415a9f9a746b332ee01a646854
GitFeri <68915209+GitFeri@users.noreply.github.com>
2020-10-31 15:18:48 +0100

    Calculator szépítése. (Teszt)

--- src/main/java/classstructureio/Calculator.java
+++ src/main/java/classstructureio/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Első szám: ");
+        System.out.println("Első szám   : ");
         int elso = scanner.nextInt();
         System.out.println("Második szám: ");
         int masodik = scanner.nextInt();
