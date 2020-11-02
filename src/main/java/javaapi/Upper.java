cf8436bbab6bdf6c29418b0455c41d9ff4abd9ae
GitFeri <68915209+GitFeri@users.noreply.github.com>
2020-11-02 23:43:00 +0100

    Upper

---
+++ src/main/java/javaapi/Upper.java
@@ -0,0 +1,9 @@
+package javaapi;
+
+public class Upper {
+    public static void main(String[] args) {
+        String string = "Hello World!";
+        string = string.toUpperCase();
+        System.out.println(string);
+    }
+}
