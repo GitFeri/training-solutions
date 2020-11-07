03f73e13a8765f10c2cac6f966ff9551dfed3b11
GitFeri <68915209+GitFeri@users.noreply.github.com>
2020-11-07 22:04:42 +0100

    Keresés

--- src/main/java/array/ArrayMain.java
+++ src/main/java/array/ArrayMain.java
@@ -15,12 +15,13 @@ public class ArrayMain {
             intArray[i] = (int) Math.pow(2, i);
         }
 
+        System.out.println("Kettő hatványai tömb elemei: ");
         for (int item : intArray) {
             System.out.println(item);
         }
 
+        System.out.println("Boolean tömb elemei :");
         Boolean[] boolArray = new Boolean[6];
-
         for (int i = 0; i < boolArray.length; i++) {
             boolArray[i] = i % 2 == 1;
         }
@@ -28,5 +29,17 @@ public class ArrayMain {
         for (Boolean b : boolArray) {
             System.out.println(b);
         }
+
+//      Keresés int tömbben.
+
+        ArrayHandler arrayHandler = new ArrayHandler();
+        int[] ints = {1, 2, 4, 6, 8, 9, 10};
+        int findIt = 6;
+
+        System.out.println("Keresés ... ");
+        System.out.println("van-e : "+ arrayHandler.contains(ints, findIt));
+        System.out.println("idex  : " + arrayHandler.find(ints,findIt));
+
+
     }
 }
