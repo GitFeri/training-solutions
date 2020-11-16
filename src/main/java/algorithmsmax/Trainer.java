cd5f4891e459c02a581b88c59f698f49bc1331e7
GitFeri <68915209+GitFeri@users.noreply.github.com>
2020-11-16 23:29:34 +0100

    java-algorithmsmax-trainer

---
+++ src/main/java/algorithmsmax/Trainer.java
@@ -0,0 +1,19 @@
+package algorithmsmax;
+
+public class Trainer {
+    String name;
+    int age;
+
+    public Trainer(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
