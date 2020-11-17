bf064b7fe797ca10362b7c116c42b902fb2370b6
GitFeri <68915209+GitFeri@users.noreply.github.com>
2020-11-17 21:54:28 +0100

    java-stringbasic-poolheap

---
+++ src/main/java/stringbasic/StringCreator.java
@@ -0,0 +1,12 @@
+package stringbasic;
+
+public class StringCreator {
+    public String createStringForPool() {
+        return "";
+    }
+
+    public String createStringForHeap() {
+        return new String();
+    }
+}
+
