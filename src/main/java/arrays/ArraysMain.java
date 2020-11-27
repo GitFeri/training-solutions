b471a02310ae8ee5e87aac42b2c29a69b4e87905
GitFeri <68915209+GitFeri@users.noreply.github.com>
2020-11-27 21:57:55 +0100

    java-arrays-daysOfWeek

--- src/main/java/arrays/ArraysMain.java
+++ src/main/java/arrays/ArraysMain.java
@@ -1,17 +1,28 @@
 package arrays;
 
+import java.time.DayOfWeek;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ArraysMain {
-    public String numberOfDaysAsString() {
-        int[] numberOfDays = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-        return Arrays.toString(numberOfDays);
-    }
-
     public static void main(String[] args) {
         ArraysMain arraysMain = new ArraysMain();
         System.out.println(arraysMain.numberOfDaysAsString());
+
+        for (int i = 0; i < 7; i++) {
+            System.out.println(arraysMain.daysOfWeek().get(i));
+
+        }
+    }
+
+    public String numberOfDaysAsString() {
+        int[] numberOfDays = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+        return Arrays.toString(numberOfDays);
+    }
+
+    List<String> daysOfWeek() {
+        return Arrays.asList("Hétfő", "Kedd", "Szerda", "Csütürtök", "Péntek", "Szombat", "Vasárnap");
     }
 
 }
