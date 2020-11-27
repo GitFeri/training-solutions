af620e7dc40ba55380a8b82eacd5b004078d87e1
GitFeri <68915209+GitFeri@users.noreply.github.com>
2020-11-27 09:15:56 +0100

    week05d04 mod

--- src/test/java/week05d04/StoreTest.java
+++ src/test/java/week05d04/StoreTest.java
@@ -2,6 +2,7 @@ package week05d04;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -10,14 +11,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class StoreTest {
     @Test
     void testStore() {
-        Store store = new Store(Arrays.asList(
+        Store store = new Store(new ArrayList<>(Arrays.asList(
                 new Product("alma", 2020, 11, 30),
                 new Product("körte", 2020, 11, 10),
                 new Product("barack", 2020, 11, 12)
-        ));
+        )));
 
         assertFalse(store.addProduct(new Product("alma", 2020, 11, 30)));
-//        assertTrue(store.addProduct(new Product("birsalma", 2020, 11, 30)));
+        assertTrue(store.addProduct(new Product("birsalma", 2021, 12, 31)));
+        assertFalse(store.addProduct(new Product("Szilva", 2020, 11, 20)));
 
 
     }
