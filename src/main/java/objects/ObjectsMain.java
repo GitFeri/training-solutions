4f53dbcdb893e210454e5a98100c6b097c8a9113
GitFeri <68915209+GitFeri@users.noreply.github.com>
2020-11-29 23:18:00 +0100

    java-objects-objektumokszama

--- src/main/java/objects/ObjectsMain.java
+++ src/main/java/objects/ObjectsMain.java
@@ -29,5 +29,23 @@ public class ObjectsMain {
 
         System.out.println(anotherBook instanceof Book);
 
+        /******************************************************************/
+
+        Book book1 = new Book();
+        Book book2 = new Book();
+        Book book3 = new Book();
+        Book book4 = book1;
+        Book book5 = book1;
+        Book book6 = book3;
+        Book book7 = null;
+        book4 = book5;
+        book5 = new Book();
+        book6 = null;
+
+        /*
+        4 opbjektum jött létre. (new)
+        Mind a 4-hez hozzá lehet férni: book1, book2, book3, book5
+         */
+
     }
 }
