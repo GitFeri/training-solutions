4cfa4b1a7a868ed38b6aa271c4e06a9b746916f1
GitFeri <68915209+GitFeri@users.noreply.github.com>
2020-12-15 16:55:13 +0100

    week08d02.Lottery v3

--- src/main/java/week08d02/Lottery.java
+++ src/main/java/week08d02/Lottery.java
@@ -5,15 +5,26 @@ import java.util.List;
 import java.util.Random;
 
 public class Lottery {
+    Random rand;
+
+    public Lottery(int seed) {
+        rand = new Random(seed);
+    }
+
+    public Lottery() {
+        rand = new Random();
+    }
+
+    int seed;
+
     public List<Integer> getNumbers(int interval, int numbers) {
         List<Integer> intervalNums = new ArrayList<>(interval);
         for (int i = 0; i < interval; i++) {
-            intervalNums.add(i);
+            intervalNums.add(i+1);
         }
 
         List<Integer> result = new ArrayList<>(numbers);
         int pos;
-        Random rand = new Random();
         for (int i = 0; i < numbers; i++) {
             pos = rand.nextInt(intervalNums.size());
             result.add(intervalNums.get(pos));
--- src/test/java/week08d02/LotteryTest.java
+++ src/test/java/week08d02/LotteryTest.java
@@ -4,5 +4,22 @@ public class LotteryTest {
     public static void main(String[] args) {
         Lottery lottery = new Lottery();
         System.out.println(lottery.getNumbers(90,5));
+        System.out.println(lottery.getNumbers(90,5));
+        System.out.println(lottery.getNumbers(90,5));
+        System.out.println(lottery.getNumbers(90,5));
+        System.out.println(lottery.getNumbers(90,5));
+        System.out.println(lottery.getNumbers(6,5));
+        System.out.println(lottery.getNumbers(6,5));
+        System.out.println(lottery.getNumbers(6,5));
+        System.out.println(lottery.getNumbers(6,5));
+        System.out.println(lottery.getNumbers(45,6));
+        System.out.println(lottery.getNumbers(45,6));
+        System.out.println(lottery.getNumbers(45,6));
+        System.out.println(lottery.getNumbers(45,6));
+        System.out.println(lottery.getNumbers(45,6));
+        System.out.println(lottery.getNumbers(45,6));
+        System.out.println(lottery.getNumbers(45,6));
+        System.out.println(lottery.getNumbers(45,6));
     }
+
 }
