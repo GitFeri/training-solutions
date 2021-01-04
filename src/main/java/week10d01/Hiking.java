746daced69f5f254385eadbaca5f0ba557faab05
GitFeri <68915209+GitFeri@users.noreply.github.com>
2021-01-04 16:05:18 +0100

    week10d01.Hiking v2

--- src/main/java/week10d01/Hiking.java
+++ src/main/java/week10d01/Hiking.java
@@ -1,11 +1,16 @@
 package week10d01;
 
+import java.util.List;
+
 public class Hiking {
     double latitude;
     double longitude;
     double height;
 
-    double getPlusElevation(double... heights) {
+    double getPlusElevation(List<Double> heights) {
+        if (heights == null) {
+            throw new IllegalArgumentException("");
+        }
         double result = 0;
         boolean first = true;
         double prev = 0;
--- src/test/java/week10d01/HikingTest.java
+++ src/test/java/week10d01/HikingTest.java
@@ -2,12 +2,20 @@ package week10d01;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HikingTest {
     @Test
     public void testGiking(){
 
-        assertEquals(13,new Hiking().getPlusElevation(10,20,15,18));
+        assertEquals(13.0,new Hiking().getPlusElevation( Arrays.asList(10.0,20.0,15.0,18.0)));
+        assertEquals(0.0,new Hiking().getPlusElevation(List.of(13.0)));
+        assertEquals(2.0,new Hiking().getPlusElevation(List.of(1.0,2.0,3.0,2.0,1.0)));
+        assertThrows(IllegalArgumentException.class, ()-> new Hiking().getPlusElevation(null));
     }
 }
