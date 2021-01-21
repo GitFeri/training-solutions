5ca9147fceae30db927e4235ea7396a340523695
GitFeri <68915209+GitFeri@users.noreply.github.com>
2021-01-21 21:50:51 +0100

    week02d02 re

---
+++ src/main/java/week02d02/PhoneMain.java
@@ -0,0 +1,32 @@
+package week02d02;
+
+import week02d02.Phone;
+
+import java.util.Scanner;
+
+public class PhoneMain {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Adja meg a 'mem'-et ");
+        int memInput = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Adja meg a 'type'-ot, ami valami egész szám");
+        String typeInput = scanner.nextLine();
+
+        Phone phone = new Phone(typeInput, memInput);
+
+        System.out.println("Ezt adta meg, mem: " + phone.getMem() + ", illetve type: " + phone.getType());
+
+        System.out.println("Adja meg az új 'mem'-et");
+        phone.setMem(scanner.nextInt());
+        scanner.nextLine();
+
+        System.out.println("Adja meg az új 'type'-ot, ami valami egész szám");
+        phone.setType(scanner.nextLine());
+
+        System.out.println("Ezt adta meg. mem: " + phone.getMem() + ", illetve type: " + phone.getType());
+    }
+}
