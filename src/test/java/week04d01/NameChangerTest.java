460033948e8404065c606749f501d45ae9abdbc3
GitFeri <68915209+GitFeri@users.noreply.github.com>
2021-01-21 22:13:06 +0100

    week04d01 re

--- src/test/java/week04d01/NameChangerTest.java
+++ src/test/java/week04d01/NameChangerTest.java
@@ -1,9 +1,7 @@
-package week04;
+package week04d01;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class NameChangerTest {
 
